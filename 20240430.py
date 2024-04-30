0d72095fca8591b3db39478cc7f2b1987977f45d
donghaeK <rlaehdgo5598@naver.com>
2024-04-30 14:35:25 +0900

    1st commit

---
+++ 20240430.py
@@ -0,0 +1,10 @@
+'''
+파이썬 실력 증진을 위한
+2024 04 30 https://wikidocs.net/7014 파이썬 300문제 시작
+'''
+print("Hello World")
+print("Mary's cosmetics")
+print('신씨가 소리질렀다. "도둑이야".')
+print("C:\Windows")
+# \t는 tap키, \n은 enter키의 역할을 한다.
+print("안녕하세요.\n만나서\t\t반갑습니다.")
