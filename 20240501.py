2280b4ad5d7e0cf7765c191f05c95ade9c967a6f
donghaeK <rlaehdgo5598@naver.com>
2024-05-01 19:51:26 +0900

    2nd commit

---
+++ 20240501.py
@@ -0,0 +1,11 @@
+'''
+파이썬 실력 증진을 위한
+2024 05 01 https://wikidocs.net/7014 파이썬 300문제 시작
+'''
+print("오늘은", "일요일")
+print("naver;kakao;sk;samsung")
+print("naver", "kakao", "sk", "samsung", sep=";")
+print("naver", "kakao", "sk", "samsung", sep="/")
+#print("first")end=print("second")
+print("first", end=""); print("second")
+print(5/3)
