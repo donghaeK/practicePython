7c03a5a3b490c6f29259bd0808701eea5dcfd75e
donghaeK <rlaehdgo5598@naver.com>
2024-05-02 20:36:21 +0900

    3rd commit

---
+++ 20240502.py
@@ -0,0 +1,23 @@
+'''
+파이썬 실력 증진을 위한
+2024 05 02 https://wikidocs.net/7014 파이썬 300문제 시작
+'''
+#1
+samsung = 50000
+samsung = samsung * 10
+print(samsung)
+#2
+시가총액 = 2980000000000
+현재가 = 50000
+PER = 15.79
+print(시가총액);print(현재가);print(PER)
+#3
+s = "hello"
+t = "python"
+print(s+"! "+t)
+#4
+print(2+2*3)
+#5
+a = "132"
+print(a)
+print(type(a))
