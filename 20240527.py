28e4a9d0d0319518031be13b686810493421e8d6
donghaeK <rlaehdgo5598@naver.com>
2024-05-27 23:43:18 +0900

    28th commit

---
+++ 20240527.py
@@ -0,0 +1,51 @@
+'''
+파이썬 실력 증진을 위한
+2024 05 27 https://wikidocs.net/7014 파이썬 300문제 시작
+'''
+
+#136 다음 코드를 for문으로 작성하라.
+# 변수 = 10
+# print(변수)
+# 변수 = 20
+# print(변수)
+# 변수 = 30
+# print(변수)
+for i in [10, 20, 30]:
+    print(i)
+
+#137 다음 코드를 for문으로 작성하라.
+# print(10)
+# print(20)
+# print(30)
+for i in [10, 20, 30]:
+    print(i)
+
+#138 다음 코드를 for문으로 작성하라.
+# print(10)
+# print("-------")
+# print(20)
+# print("-------")
+# print(30)
+# print("-------")
+for i in [10, 20, 30]:
+    print(i)
+    print("-------")
+
+#139 다음 코드를 for문으로 작성하라.
+# print("++++")
+# print(10)
+# print(20)
+# print(30)
+print("++++")
+for i in [10, 20, 30]:
+    print(i)
+
+#140 다음 코드를 for문으로 작성하라.
+# print("-------")
+# print("-------")
+# print("-------")
+# print("-------")
+
+for i in range(4):
+    print("-------")
+
