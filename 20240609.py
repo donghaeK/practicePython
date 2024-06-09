bdd89904ee3c0a85ff3cc259d40e7c5d1b0e4bd0
donghaeK <rlaehdgo5598@naver.com>
2024-06-09 21:10:20 +0900

    41th commit

---
+++ 20240609.py
@@ -0,0 +1,41 @@
+'''
+파이썬 실력 증진을 위한
+2024 06 09 https://wikidocs.net/7014 파이썬 300문제 시작
+'''
+
+# 201
+# "비트코인" 문자열을 화면에 출력하는 print_coin() 함수를 정의하라.
+# def print_coin():
+#     print("비트코인")
+# 202
+# 201번에서 정의한 함수를 호출하라.
+#print_coin
+
+# 203
+# 201번에서 정의한 print_coin 함수를 100번호출하라.
+#for i in range(100):
+#    print_coin()
+print("===")
+# 204
+# "비트코인" 문자열을 100번 화면에 출력하는 print_coins() 함수를 정의하라.
+def print_coins():
+    for i in range(100):
+        print("비트코인")
+
+print_coins()
+
+# 205
+# 아래의 에러가 발생하는 이유에 대해 설명하라.
+#
+# hello()
+# def hello():
+#     print("Hi")
+# 실행 예
+#
+# NameError: name 'hello' is not defined
+
+#함수가 정의되기전에 호출되었기 때문이다.
+
+
+
+
